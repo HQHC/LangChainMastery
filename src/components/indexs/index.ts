700043453373ceb9661387c57a7509c6d0230b8f
清魄 <meng.mfb@alibaba-inc.com>
2023-07-07 13:55:03 +0800

    fix: 添加魈

--- src/components/indexs/index.ts
+++ src/components/indexs/index.ts
@@ -114,6 +114,83 @@ export class APIController {
     console.log(reaction4);
   }
 
+  @Get('/xiaoVector')
+  async xiaoVector() {
+    const oenpAIConfig = {
+      basePath: process.env.BASE_PATH,
+    };
+
+    const llm = new OpenAI(
+      {
+        temperature: 0.9,
+        maxTokens: 1500,
+      },
+      oenpAIConfig
+    );
+
+    const loader = new DirectoryLoader('src/text/魈', {
+      '.txt': path => new TextLoader(path),
+    });
+
+    const docs = await loader.load();
+
+    const vectorStore = new MemoryVectorStore(
+      new OpenAIEmbeddings({}, oenpAIConfig)
+    );
+
+    const retriever = new TimeWeightedVectorStoreRetriever({
+      vectorStore,
+      otherScoreKeys: ['importance'],
+      k: 15,
+    });
+
+    // await retriever.addDocuments(docs);
+
+    const linghuaMemory: GenerativeAgentMemory = new GenerativeAgentMemory(
+      llm,
+      retriever,
+      { reflectionThreshold: 8 }
+    );
+
+    const linghua: GenerativeAgent = new GenerativeAgent(llm, linghuaMemory, {
+      name: '魈',
+      age: 25,
+      traits: '勇敢果断、沉默寡言',
+      status: '',
+    });
+
+    await Promise.all(
+      docs.map(({ pageContent }) => {
+        return linghua.memory.addMemory(pageContent);
+      })
+    );
+
+    const [, reaction1] = await linghua.generateReaction('你好呀，魈');
+    // console.log(reaction1, 'reaction')
+
+    const [, reaction2] = await linghua.generateReaction('最近忙啥呢？');
+
+    const [, reaction3] = await linghua.generateReaction('你怎么看待996？');
+
+    const [, reaction4] = await linghua.generateReaction('生命的意义是什么呢？')
+
+    // const [, reaction1] = await linghua.generateDialogueResponse(
+    //   '你好呀，凌华'
+    // );
+    console.log(reaction1);
+
+    // const [, reaction2] = await linghua.generateDialogueResponse(
+    //   '最近忙啥呢？'
+    // );
+    console.log(reaction2);
+    // return await linghua.getSummary({ forceRefresh: true });
+
+    console.log(reaction3);
+
+    console.log(reaction4);
+  }
+
+
   @Get('/hnswlib')
   async hnswlib() {
     const loader = new TextLoader('src/text/神里凌华.txt');
